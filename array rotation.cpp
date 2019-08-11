a45b458ae0ff464150248f83a66bf4bd522959ed
Shivam Mishra <41479737+codder-shivam@users.noreply.github.com>
2019-08-11 14:41:29 +0530

    Update and rename rotate arr by D.cpp to array rotation.cpp
    to rotate D elements of any random unsorted array of index N clockwise.
     i.e. if array is:       [9,8,6,2,10,19,80] (N=7) which  on rotation of 3 elements (D=2) will result in 
    [2,10,19,80,9,8,6] .

--- array rotation.cpp
+++ array rotation.cpp
@@ -1,4 +1,4 @@
-
+// program to rotate "d" elements of array clockwise 
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -6,12 +6,14 @@ using namespace std;
 int main()
  {
 	//code
-	int  t,d,n;
+	int  t,d,n;//t= testcase, d=elements to be rotated , n= size of array
 	cin>>t;
-	while(t--)
+	while(t--)      //for running testcases
 	{
 	    cin>>n>>d;
-	    int arr[n];
+		//array of index n
+	    int arr[n]; 
+		// temporary index created to hold values to rotate
 	    int temp[d];
 	    for(int i=0; i<n; i++) cin>>arr[i];
 	    for(int i=0; i<d; i++) temp[i]=arr[i];
